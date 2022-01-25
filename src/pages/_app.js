7fa963a0a1315d3d9b0a94a677f22038adbdab84
croschel <croschel000@gmail.com>
2022-01-25 12:58:21 -0300

    on submit is ready

---
+++ src/pages/_app.js
@@ -0,0 +1,40 @@
+function GlobalStyle() {
+  return (
+    <style global jsx>
+      {`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+        }
+        body {
+          font-family: "Open Sans", sans-serif;
+        }
+        html,
+        body,
+        #__next {
+          min-height: 100vh;
+          display: flex;
+          flex: 1;
+        }
+        #__next {
+          flex: 1;
+        }
+        #__next > * {
+          flex: 1;
+        }
+      `}
+    </style>
+  );
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,7 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
-import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>
-      {`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-        body {
-          font-family: "Open Sans", sans-serif;
-        }
-        html,
-        body,
-        #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-      `}
-    </style>
-  );
-}
+import { useRouter } from "next/router";
+import { useState } from "react";
+import appConfig from "../../config.json";
 
 function Titulo(props) {
   const Tag = props.tag || "h1";
@@ -48,11 +20,19 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = "croschel";
+  const [githubName, setGithubName] = useState();
+  const router = useRouter();
 
+  const handleChange = (event) => {
+    setGithubName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    router.push("/chat");
+  };
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -87,6 +67,7 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={handleSubmit}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -110,6 +91,8 @@ export default function PaginaInicial() {
 
             <TextField
               fullWidth
+              value={githubName}
+              onChange={handleChange}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -154,7 +137,7 @@ export default function PaginaInicial() {
                 borderRadius: "50%",
                 marginBottom: "16px",
               }}
-              src={`https://github.com/${username}.png`}
+              src={`https://github.com/${githubName}.png`}
             />
             <Text
               variant="body4"
@@ -165,7 +148,7 @@ export default function PaginaInicial() {
                 borderRadius: "1000px",
               }}
             >
-              {username}
+              {githubName}
             </Text>
           </Box>
           {/* Photo Area */}
